5ca71f74fc94e7c5510477f815dcd5a33ea8da22
Alspirid <Aleksey.Spiridonov@gmail.com>
2018-04-04 17:40:36 -0700

    Add pyramid iterative and recursice solutions

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,31 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+// function pyramid(n) {
+//   let blocks, space;
+//   for (let i = 1; i <= n; i++) {
+//       blocks = (i * 2 -1);
+//       space = (n * 2 - 1 - blocks)/2; 
+//       console.log(' '.repeat(space) + '#'.repeat(blocks)+ ' '.repeat(space));    
+//   }
+// }
+
+function pyramid (n, k = 1) {
+  let blocks, space;
+  blocks = (k * 2 - 1);
+  space = (n * 2 - 1 - blocks)/2;
+  if ( k === n) {
+    console.log(' '.repeat(space) + '#'.repeat(blocks)+ ' '.repeat(space));
+    return;
+  }
+  console.log(' '.repeat(space) + '#'.repeat(blocks)+ ' '.repeat(space));
+  pyramid(n, k + 1);
+}
+
 
 module.exports = pyramid;
+
+
+// '  #  '
+// ' ### '
+// '#####'
