80da8112b1bc771bef4ab7418e00ee2e255c8b98
Alspirid <Aleksey.Spiridonov@gmail.com>
2018-04-11 16:45:33 -0700

    Add fibonacci recursive, iterative and dynamic

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,62 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function fib(n) {
+  if ( n <= 1) {
+    return n;
+  }
+  if (n === 2) {
+    return 1;
+  }
+  return fib(n-1) + fib(n-2);
+}
+
+function fib_sum(n) {
+  if ( n === 0) {
+    return [0];
+  }
+  if ( n === 1) {
+    return [0,1];
+  }
+  if (n === 2) {
+    return [0,1,1];
+  }
+  let sum = fib_sum(n-1);
+  sum.push(sum[sum.length-2] + sum[sum.length-1]);
+  return sum;
+}
+  
+function totals(n) {
+  const arr = fib_sum(n);
+  return arr.reduce((total, elem)=> total + elem);
+}  
+
+
+const iterativeFibs = n => {
+  const fibsArray = [0,1,1];
+  if ( n ===0 || fibsArray[n]) {
+    return fibsArray[n];
+  }
+  for (let i = 3; i <= n; i++) {
+    let last =  fibsArray[fibsArray.length-2]+fibsArray[fibsArray.length-1];
+    fibsArray.push(last);
+  }
+  return fibsArray[n];
+};
+
+class Fibonacci {
+  constructor(){
+    this.fibs = [0,1];
+  }
+  fibSumDynamic(n) {
+    if ( n === 0 || this.fibs[n]) {
+      return this.fibs[n];
+    }
+    let value =  this.fibSumDynamic(n-1) + this.fibSumDynamic(n-2);
+    this.fibs[n] = value;
+    console.log(this.fibs);
+    return value;
+  }
+}
 
 module.exports = fib;
