5efcd052f15624ab80993df06f8129ce72a4428d
Alspirid <Aleksey.Spiridonov@gmail.com>
2018-04-23 22:06:30 -0700

    Add breadth first checking of the binary level's width

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,21 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  const result = [0];
+  const arr = [root,'s'];
+
+    while (arr.length > 1){
+      const node = arr.shift();
+      if (node === 's'){
+        result.push(0);
+        arr.push('s');  
+      } else {
+        arr.push(...node.children);
+        result[result.length-1]++;
+      }
+    }
+  return result;
+}
 
 module.exports = levelWidth;
